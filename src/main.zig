43e3f8a25602f0a729c2d5bc266d0d6690838998
vic-Rokx <v.rokx.nellemann@gmail.com>
2025-11-12 15:01:14 -0500

    testing

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 const loompkg = @import("loom");
 
-const resp = "HTTP/1.1 200 OK\r\nDate: Tue, 19 Aug 2025 18:37:36 GMT\r\nContent-Length: 7\r\nContent-Type: text/plain charset=utf-8\r\n\r\nSUCCESS";
+const resp = "HTTP/1.1 200 OK\r\nDate: Tue, 19 Aug 2025 18:37:36 GMT\r\nContent-Length: 1000000\r\nContent-Type: text/plain charset=utf-8\r\n\r\n";
 var payload: []u8 = undefined;
 var allocator: std.mem.Allocator = undefined;
 var local_buffer: [8192]u8 = undefined;
 const xsuspend = loompkg.xsuspend;
 fn handle(client: *loompkg.Client, _: []const u8) !void {
-    try client.chunked(resp);
+    try client.chunked(payload);
 }
 
 pub fn makePayload(size: usize) ![]u8 {
@@ -19,9 +19,9 @@ pub fn makePayload(size: usize) ![]u8 {
 pub fn main() !void {
     allocator = std.heap.page_allocator;
 
-    payload = try allocator.alloc(u8, (resp.len + 10000000));
+    payload = try allocator.alloc(u8, (resp.len + 1000000));
     @memcpy(payload[0..resp.len], resp);
-    @memcpy(payload[resp.len..(resp.len + 10000000)], try makePayload(10000000));
+    @memcpy(payload[resp.len..(resp.len + 1000000)], try makePayload(1000000));
 
     const config = loompkg.Loom.Config{
         .server_addr = "0.0.0.0",
